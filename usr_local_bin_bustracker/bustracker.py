12138cb58be367b0ea684f512ef14c800950f414
falloutphil <falloutphil@example.com>
2023-01-22 14:46:51 +0000

    refactor so can get 2 buses for 3 routes on the grid

--- usr_local_bin_bustracker/bustracker.py
+++ usr_local_bin_bustracker/bustracker.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+# python2 - remember!
+
 from Adafruit_8x8 import EightByEight
 from time import sleep
 from urllib import urlopen
 from json import loads
 from sys import stderr
+from itertools import izip_longest, cycle
 
 font = {
 
@@ -92,6 +95,13 @@ font = {
  [1,0,0],
  [1,1,1]
 ],
+'?':[
+ [0,0,0],
+ [0,0,0],
+ [0,1,0],
+ [0,0,0],
+ [0,0,0]
+],
 '*' : [
  [1,1,1,1,1,1,1,1],
  [1,0,0,0,0,0,1,1],
@@ -101,10 +111,6 @@ font = {
 ]
 
 }
-
-
-# https://api.tfl.gov.uk/Line/E3,440,N11/Arrivals/490010968V?direction=outbound&app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY
-# [{"$type":"Tfl.Api.Presentation.Entities.Prediction, Tfl.Api.Presentation.Entities","id":"278132595","operationType":1,"vehicleId":"SK07DYC","naptanId":"490010968V","stationName":"Petersfield Road","lineId":"440","lineName":"440","platformName":"V","direction":"outbound","bearing":"158","destinationNaptanId":"","destinationName":"Chiswick Power Road","timestamp":"2016-07-31T13:53:36Z","timeToStation":1115,"currentLocation":"","towards":"Turnham Green","expectedArrival":"2016-07-31T14:12:11.3563248Z","timeToLive":"2016-07-31T14:12:41.3563248Z","modeName":"bus"}]
 
 def handleDue( estimatedTime ):
     if estimatedTime == '0':
@@ -117,35 +123,46 @@ def printToMatrix( grid, stringInList, starty=0, startx=0 ):
             for xpos,bit in enumerate(bitRow,startx+letter*len(bitRow)+letter):
                 grid.setPixel(ypos,xpos,bit) 
 
+# Take the json and iterate through filter for the bus in question and
+# return "count" nearest results in minutes
+# Inputs are strings so convert to ints for sort
+def get_bus_times(*rest_urls, **kwargs):
+    count = kwargs.get('count', 2)
+    json_data_list = [loads(response.read()) for response in [ urlopen(url) for url in rest_urls]]
+    return [sorted([str(buses['timeToStation']/60) for buses in data], key=int)[:count] for data in json_data_list]
 
-
+# Dot Matrix grid is split into two vertical columns
 lhs = EightByEight(address=0x70)
 rhs = EightByEight(address=0x71)
 
-url = 'https://api.tfl.gov.uk/line/e3,440,n11/arrivals/490010968V?direction=outbound&app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY'
+# Use this to find a bus stop of interest
+# https://tfl.gov.uk/tfl/syndication/feeds/bus-stops.csv
+# Petersfield Road
+url_e3_pr  = 'https://api.tfl.gov.uk/line/e3/arrivals/490010968V?direction=outbound&app_id=XXXX&app_key=XXXX'
+# Meon Road
+url_440_mr = 'https://api.tfl.gov.uk/line/440/arrivals/490010968W?direction=outbound&app_id=XXXX&app_key=XXXX'
+# 490003083G ACTON OLD TOWN HALL (westbound)
+url_266_th_west = 'https://api.tfl.gov.uk/line/266/arrivals/490003083G?direction=outbound&app_id=XXXX&app_key=XXXX'
+
 
 while True:
     try:
-        response = urlopen(url);
-        data = loads(response.read())
-        #print data
-        #print '------------------------------------'
-	nextThreeE3 = sorted([str(buses['timeToStation']/60) for buses in data if buses['lineId'] == 'e3'],key=int)[:3]
-	nextTwo440 = sorted([str(buses['timeToStation']/60) for buses in data if buses['lineId'] == '440'],key=int)[:2]
-	nextOneN11 = sorted([str(buses['timeToStation']/60) for buses in data if buses['lineId'] == 'N11'],key=int)[:1]
+        next_buses_by_route = get_bus_times(url_e3_pr, url_440_mr, url_266_th_west)
         lhs.clear()
         rhs.clear()
         ypos=0
-        if len(nextThreeE3) > 0:
-            for ymod,wait in enumerate(nextThreeE3):
-                printToMatrix( lhs, wait, ypos )
-                ypos+=(6-ymod)
-        ypos=0
-        if len(nextTwo440) > 0:
-            for ymod,wait in enumerate(nextTwo440):
-                printToMatrix( rhs, wait, ypos )
-                ypos+=(6-ymod)
-        if len(nextOneN11) > 0: printToMatrix( rhs, nextOneN11.pop(), 11, 1 )
+        # split buses into vertical columns, where each row represents a route.
+        # if any route has less than 3 buses due, add a '?' to keep each row earmarked for
+        # a specific route.
+        buses = izip_longest(*next_buses_by_route, fillvalue='??')
+        # iterator that flips between left and right hand grid objects
+        sides = cycle([lhs,rhs])
+        # enumerate to get a y-axis coordinate reference
+        for bus_column, get_side in zip([enumerate(bus) for bus in buses], sides):
+            for ymod, wait in bus_column:
+               printToMatrix(get_side, wait, ypos)
+               ypos+=(6-ymod)
+            ypos=0
     except:
         print >> stderr, 'Ooops Python Threw-up, handling and restarting loop...'
     
